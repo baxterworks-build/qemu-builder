823a2c0f001c0d4fa3e5d7453a8c3a9ce7061fd3
Adam Baxter <voltagex@voltagex.org>
2020-08-09 14:22:56 +1000

    Enable x86 and arm targets

--- drone-helper.sh
+++ drone-helper.sh
@@ -26,9 +26,21 @@ fi
 echo "Staging is $STAGING"
 echo "Working dir is $WORKING"
 
+#targets
+#aarch64-softmmu alpha-softmmu
+#arm-softmmu avr-softmmu cris-softmmu hppa-softmmu
+#i386-softmmu lm32-softmmu m68k-softmmu
+#microblaze-softmmu microblazeel-softmmu mips-softmmu
+#mips64-softmmu mips64el-softmmu mipsel-softmmu
+#moxie-softmmu nios2-softmmu or1k-softmmu ppc-softmmu
+#ppc64-softmmu riscv32-softmmu riscv64-softmmu
+#rx-softmmu s390x-softmmu sh4-softmmu sh4eb-softmmu
+#sparc-softmmu sparc64-softmmu tricore-softmmu
+#unicore32-softmmu x86_64-softmmu xtensa-softmmu
+#xtensaeb-softmmu
 
-export CFLAGS="-Wno-stringop-truncation"
-./configure --python=$(command -v python3) --cross-prefix=x86_64-w64-mingw32- --disable-docs --enable-whpx
+ENABLED_TARGETS="aarch64-softmmu,arm-softmmu,i386-softmmu,x86_64-softmmu"
+./configure --python=$(command -v python3) --cross-prefix=x86_64-w64-mingw32- --disable-docs --enable-whpx --target-list=$ENABLED_TARGETS
 echo 5.99.99 > VERSION
 JOBS=${JOBS:=$(nproc)} #if we don't pass a JOBS variable in, use the value of nproc 
 echo Number of jobs set to $JOBS!
@@ -48,6 +60,10 @@ THIRD=$(for d in $SECOND; do strings $d | grep '\.dll' | sort -u | xargs -I{} re
 echo $FIRST $SECOND $THIRD | sed 's/ /\n/g' | sort -u | xargs -I{} cp -v {} $STAGING
 
 pushd /tmp/myqemu/
+echo
+echo
 tar -czf - . | curl -vL -F file=@- https://tmp.ninja/api.php?d=upload-tool
+echo
+echo
 popd
 
